a09d67a20be81407dc681fcc3fb500b3dc2af4b2
jonylu7 <jonylu7@gmail.com>
2024-09-13 19:32:37 +0800

    run

--- ORAgent/main.py
+++ ORAgent/main.py
@@ -2,12 +2,12 @@ import json
 import ConvertDataFormat
 import OR
 import ORExport
-graph_file_location= "og_data/waypoint_graph.json"
-orders_file_location= "og_data/orders_data.json"
-vehicle_file_location="og_data/vehicle_data.json"
-export_file_location="og_data/OR_response.json"
+graph_file_location= "./og_data/waypoint_graph.json"
+orders_file_location= "./og_data/orders_data.json"
+vehicle_file_location="./og_data/vehicle_data.json"
+export_file_location="./og_data/OR_response.json"
 
-def main():
+def runORAgent():
     ##preprocess
     offsets,edges,weights,orders=ConvertDataFormat.preprocess(graph_file_location,orders_file_location)
 
@@ -16,9 +16,9 @@ def main():
 
     solution,totalCost=OR.solveTSP(costmatrix,orders)
     solutionPath=OR.generateSolutionPath(pathmatrix,solution)
-    ORExport.exportFormat(solutionPath,totalCost)
+    ORExport.exportFormat(solutionPath,totalCost,export_file_location)
     ##costmatrix as argument
 
 
 if __name__=="__main__":
-    main()
+    runORAgent()
